be50dfd7995eb7a457e4a69fe51ddb18b421818a
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-12-22 11:46:10 +0800

    add suspense

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,8 +1,14 @@
 'use client'
 import _ from 'lodash'
 import { clsx } from 'clsx'
+import Test from '@/component/test'
+import Boundary from '@/component/Boundary'
 interface Props {}
 
 export default function Page({}: Props) {
-  return <div> in page</div>
+  return <div>
+    <Boundary>
+      <Test></Test>
+    </Boundary>
+  </div>
 }
