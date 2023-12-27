e8eab7ff249e1a4782505dea0a2b0d7f9f21d9e8
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2023-12-27 15:57:46 +0800

    test infiniteSWR

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,32 +1,8 @@
 'use client'
 import _ from 'lodash'
 import { clsx } from 'clsx'
-import useSWR from 'swr'
-import { testAction } from '@/server-actions/testAction'
-import { useState } from 'react'
 interface Props {}
 
 export default function Page({}: Props) {
-  const [time, setTime] = useState(1)
-  const { data, isLoading, isValidating } = useSWR(
-    {
-      url: '/testAction',
-      time: time,
-    },
-    testAction,
-  )
-  if (isLoading) return <div>loading</div>
-  console.log('data', data)
-  return (
-    <div>
-      in page
-      <button
-        onClick={() => {
-          setTime(time + 1)
-        }}
-      >
-        click
-      </button>
-    </div>
-  )
+  return <div></div>
 }
